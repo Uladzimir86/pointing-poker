a6dce50bc02307601f5230c4b53d8110174c2425
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-09-04 01:15:58 +0300

    feat: import font fixed

--- front/src/UI-components/player-card/player-card.tsx
+++ front/src/UI-components/player-card/player-card.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './player-card.scss';
 
-const PlayerCard = () => {
-  const foto = 'AS';
-  const name = 'Egor Dilda';
-  const position = 'junior developer';
-  const btn = false;
-  const above = false;
+interface Player {
+  photo?: string,
+  name?: string,
+  position?: string,
+  btnDelPlayer?: boolean,
+  above?: boolean,
+}
+const PlayerCard = ({photo = 'PH', name = 'Name', position = 'Position', btnDelPlayer = false, above = false}: Player) => {
   const deletePlayerCard = () => {
     console.log('deletePlayerCard');
   }
   return (
     <div className="player-card">
-      <div className="player-card__foto-container">
-        <div className="player-card__foto">
-          {foto}
+      <div className="player-card__photo-container">
+        <div className="player-card__photo">
+          {photo}
         </div> 
       </div>
       <div className="player-card__text-container">
@@ -22,7 +24,7 @@ const PlayerCard = () => {
         <div className="player-card__name">{name}</div>
         <div className="player-card__position">{position}</div>
       </div>
-      <button type="button" className="player-card__button" onClick={deletePlayerCard} hidden={btn}></button>
+      <button type="button" className="player-card__button" onClick={deletePlayerCard} hidden={btnDelPlayer}></button>
     </div>
   )
 }
