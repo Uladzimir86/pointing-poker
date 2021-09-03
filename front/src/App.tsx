e52d4a8782c24fc3dbea1d9c9ccb77a7d4aa6251
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-09-03 17:53:04 +0300

    feat: create footer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../src/UI-components/header/header';
+import Footer from '../src/UI-components/footer/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
