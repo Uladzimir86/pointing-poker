1e8772100337d62c86d9ac1408768c7c78a790a0
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-09-04 16:07:22 +0300

    feat: create switcher

--- front/src/UI-components/switcher/switcher.tsx
+++ front/src/UI-components/switcher/switcher.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
+import { useState } from 'react';
 import './switcher.scss';
 
 const Switcher = () => {
-  return (
-    <div className="switcher">
-      <div className="slider-wrapper">
-      {/* <input type="range"/> */}
-  <input className="slider" type="range" min="0" max="1" step="1" />
-</div>
-    </div>
-  )
+  const [switcherOn, setSwitcherOn] = useState(true)
+  const handleSwitcher = () => {
+    setSwitcherOn(!switcherOn);
+  }
+  return <input className={`switcher ${switcherOn && 'switcher_on'}`} type="range" min="0" max="1" step="1" onChange={handleSwitcher}/>
 }
 
 export default Switcher;
