062badd845ab2772f701dd0b8b08c5b1a6fb5824
Baiadil <adikmain@gmail.com>
2021-09-05 02:52:02 +0600

    [ADD] Made dropdown menu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,10 @@
+import CustomDropdown from './UI-components/custom-dropdown/custom-dropdown.component'
 
 function App() {
   return (
     <div className="App">
+      <CustomDropdown />
+      <h1>hello world</h1>
     </div>
   )
 }
