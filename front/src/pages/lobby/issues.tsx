2d812dfd19f5b8a6dc45f45f91f7e1c6a9d32e47
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-09-06 18:14:14 +0300

    create component issues for page lobby

---
+++ front/src/pages/lobby/issues.tsx
@@ -0,0 +1,40 @@
+import './issues.scss';
+import CustomIssue, { CustomIssueInterface } from '../../UI-components/custom-issue/custom-issue.component';
+
+interface IIssues {
+  arrOfIssues: CustomIssueInterface[],
+}
+
+const Issues: React.FC<IIssues> = ({arrOfIssues}) => {
+  const issues = arrOfIssues.map(({
+    deleteButton,
+    editButton,
+    createButton,
+    closeButton,
+    currentCard,
+    priority,
+    number,}) => {
+ 
+    return (
+      <CustomIssue
+      deleteButton={deleteButton}
+      editButton={editButton}
+      createButton={createButton}
+      closeButton={closeButton}
+      currentCard={currentCard}
+      priority={priority}
+      number={number}
+      />
+    )
+  })
+  return (
+    <div className="issues">
+      <span className="title">Issues</span>
+      <div className="issues__container">
+        {issues}
+      </div>
+    </div>
+  )
+}
+
+export default Issues;
