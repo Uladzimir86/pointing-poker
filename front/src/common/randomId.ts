d22a3458f02dd83ccf6bb1be5168873cd255537e
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-09-22 13:40:57 +0300

    delete unnecessary files

--- front/src/common/randomId.ts
+++
@@ -1,6 +0,0 @@
-import { uid } from "uid"
-
-export function createID():string{
-    const lengthId = 3
-    return uid(lengthId)
-}
--- front/src/store/reducers/alert/alertReducer.ts
+++
@@ -1,9 +0,0 @@
-export const setAlert = (state = '', action: any) => {
-  switch (action.type) {
-    case 'SHOW_ALERT':
-      return action.payload;
-    case 'HIDE_ALERT':
-      return '';
-    default: return state;
-  }
-}
