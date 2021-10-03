c4a349ba7d019e39c7f1dfc8496a77819ddeadda
Vlad Yanushevsky <yanushevskyv@gmail.com>
2021-10-04 01:11:02 +0300

    add styles  for chat

--- front/src/pages/ChatModule/Chat.tsx
+++ front/src/pages/ChatModule/Chat.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './chat.scss'
+import PlayerCard from '../../UI-components/player-card/player-card'
 
 
 type PropsChat = {
@@ -8,6 +9,21 @@ type PropsChat = {
 export const Chat : React.FC<PropsChat>= ({showChatbar})=>{
 
     return <div id='#chatbar' className = {showChatbar? 'chatbar': 'chatbar disabled'}>
-
+            <div className="chat">
+                <div className="chat_row">
+                    <div className="chat_row__msg">
+                        <div className="chat_row__msg_text">
+                            Vladimir how are you? Where is the server?
+                        </div>
+                    </div>
+                    <div className="chat_row__user">
+                        <PlayerCard btnDelPlayer={false} name={'MrBean'}/>
+                    </div>
+                </div>
+                <div className="chat_control">
+                    <input type="text" name="msg" id="input_msg" />
+                    <button className="chat_control_btn">send</button>
+                </div>
+            </div>
     </div>
 }
